1532d69fd6c2128122c1bee621aacd8c36ce8193
Ricardo de Paula <ricardo.paula@sodobrasil.net.br>
2021-08-23 14:44:28 -0300

    finish: Test of HashPassword

--- tests/HashPassword.test.ts
+++ tests/HashPassword.test.ts
@@ -3,8 +3,8 @@ import BcryptAdapter from '../src/adapters/cryptography/BcryptAdapter'
 test('Hash password', async () => {
     const password = '123456'
     const bcryptAdapter = new BcryptAdapter()
-
+    
     const hashedPass = await bcryptAdapter.getHashed(password)
-
+    expect(password).not.toBe(hashedPass)
     expect(await bcryptAdapter.compare(password,hashedPass)).toBeTruthy()
 })
