4bdc7cd14028a8d22be4d9363bb331a0e9227e7a
ricardobpaula <flashdepaula@gmail.com>
2021-08-24 23:20:20 -0300

    add: Phone test added

---
+++ src/modules/accounts/entities/Phone.test.ts
@@ -0,0 +1,17 @@
+import Phone from "./Phone"
+import InvalidPhoneError from "./errors/InvalidPhoneError"
+
+describe('Phone value', () => {
+    
+    it('should phone valid value',()=>{
+        const phone = new Phone('11999998888')
+        expect(phone.phone).toBe('11999998888')
+    })
+
+    it('should be not accepted with length different from 11',()=>{
+        const error = () => {
+            new Phone('011999998888')
+        }
+        expect(error).toThrowError(InvalidPhoneError)
+    })
+})
