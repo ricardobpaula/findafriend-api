063ed88be08ad7f4eeb3dc2f27651fe5c5fc74a5
ricardobpaula <flashdepaula@gmail.com>
2021-10-04 01:39:59 -0300

    fix: columns names

--- prisma/migrations/20211004042310_create_specie_and_pet/migration.sql
+++ prisma/migrations/20211004042310_create_specie_and_pet/migration.sql
@@ -15,17 +15,17 @@ CREATE TABLE "pets" (
     "description" TEXT NOT NULL,
     "adopted" BOOLEAN NOT NULL DEFAULT false,
     "size" "Size" NOT NULL DEFAULT E'undefined',
-    "ownerId" INTEGER NOT NULL,
-    "specieId" INTEGER NOT NULL,
+    "owner_id" INTEGER NOT NULL,
+    "specie_id" INTEGER NOT NULL,
 
     CONSTRAINT "pets_pkey" PRIMARY KEY ("id")
 );
 
 -- AddForeignKey
-ALTER TABLE "pets" ADD CONSTRAINT "pets_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "pets" ADD CONSTRAINT "pets_owner_id_fkey" FOREIGN KEY ("owner_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "pets" ADD CONSTRAINT "pets_specieId_fkey" FOREIGN KEY ("specieId") REFERENCES "species"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "pets" ADD CONSTRAINT "pets_specie_id_fkey" FOREIGN KEY ("specie_id") REFERENCES "species"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- RenameIndex
 ALTER INDEX "users.email_unique" RENAME TO "users_email_key";
