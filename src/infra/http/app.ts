7d2a45b63002fd8571fe17ee213fe6670ae4d6cd
ricardobpaula <flashdepaula@gmail.com>
2021-10-15 13:33:21 -0300

    feat: cors

--- src/infra/http/app.ts
+++ src/infra/http/app.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 import dotenv from 'dotenv'
 import routes from './routes/index.routes'
@@ -7,6 +9,15 @@ dotenv.config()
 
 const app = express()
 
+const corsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 200
+}
+
+app.use(cookieParser())
+app.use(cors(corsOptions))
 app.use(express.json())
 
 app.use('/', routes)
